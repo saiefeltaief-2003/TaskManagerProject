cb75f6ff592dd5c079942fb5621609212ad9f9ee
saiefeltaief-2003 <saief.eltaief@gmail.com>
2026-05-13 23:34:12 +0100

    backend: get by id, update, delete and pushing and pulling from user

--- Backend/controllers/taskController.js
+++ Backend/controllers/taskController.js
@@ -7,18 +7,10 @@ exports.createTask = async (req, res) =>
     {
         const { name, description, user } = req.body;
         const newTask = await taskModel.create({name, description, user});
-        let taskUser = await userModel.findById(user);
-        if (!taskUser)
-        {
-            res.status(400).json(
-                {
-                    message: "No user with ID found.",
-                    error: error,
-                }
-            );
-        };
-        taskUser.tasks.push(newTask);
-        await userModel.updateOne(taskUser);
+        await userModel.findOneAndUpdate(
+            {_id: user},
+            {$push: {tasks: newTask._id}}
+        );
         res.status(201).json(
             {
                 message: "Task created.",
@@ -64,81 +56,104 @@ exports.getAllTasks = async (req, res) =>
     }
 }
 
-// // findById finds one user with a given id.
-// // req.params is the url params. Look in the routes to see how the id is set up as a path parameter.
-// // Conventionally should return a response of status code 200 (OK).
-// exports.getUserById = async (req, res) =>
-// {
-//     try
-//     {
-//         const user = await userModel.findById(req.params.id);
-//         res.status(200).json(
-//             {
-//                 message: "Fetched user.",
-//                 data:
-//                 {
-//                     user
-//                 },
-//             }
-//         );
-//     }
-//     catch (error)
-//     {
-//         res.status(400).json(
-//             {
-//                 message: "Request failed.",
-//                 error: error,
-//             }
-//         );
-//     }
-// }
+exports.getTaskById = async (req, res) =>
+{
+    try
+    {
+        const task = await taskModel.findById(req.params.id);
+        res.status(200).json(
+            {
+                message: "Fetched task.",
+                data:
+                {
+                    task
+                },
+            }
+        );
+    }
+    catch (error)
+    {
+        res.status(400).json(
+            {
+                message: "Request failed.",
+                error: error,
+            }
+        );
+    }
+}
 
-// // findByIdAndUpdate finds a user by id and updates it with the body of the request.
-// // The new option lets the response be the user post-update instead of pre-update.
-// // The runValidators option allows the field validators to be executed.
-// // Conventionally should return a response of status code 200 (OK).
-// exports.updateUser = async (req, res) =>
-// {
-//     try
-//     {
-//         const user = await userModel.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
-//         res.status(200).json(
-//             {
-//                 message: "Updated user.",
-//                 data:
-//                 {
-//                     user
-//                 },
-//             }
-//         );
-//     }
-//     catch (error)
-//     {
-//         res.status(400).json(
-//             {
-//                 message: "Request failed.",
-//                 error: error,
-//             }
-//         );
-//     }
-// }
+exports.updateTask = async (req, res) =>
+{
+    try
+    {
+        const task = await taskModel.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(200).json(
+            {
+                message: "Updated task.",
+                data:
+                {
+                    task
+                },
+            }
+        );
+    }
+    catch (error)
+    {
+        res.status(400).json(
+            {
+                message: "Request failed.",
+                error: error,
+            }
+        );
+    }
+}
 
-// // findByIdAndDelete finds a user by id and deletes it.
-// // Conventionally should return a response of status code 204 (No Content) without a message.
-// exports.deleteUser = async (req, res) =>
-// {
-//     try
-//     {
-//         await userModel.findByIdAndDelete(req.params.id);
-//         res.status(204).json();
-//     }
-//     catch (error)
-//     {
-//         res.status(400).json(
-//             {
-//                 message: "Request failed.",
-//                 error: error,
-//             }
-//         );
-//     }
-// }
+exports.deleteTask = async (req, res) =>
+{
+    try
+    {
+        const taskToDelete = await taskModel.findById(req.params.id);
+        await userModel.findOneAndUpdate(
+            {_id: taskToDelete.user},
+            {$pull: {tasks: req.params.id}}
+        )
+        await taskModel.deleteOne(taskToDelete);
+        res.status(204).json();
+    }
+    catch (error)
+    {
+        res.status(400).json(
+            {
+                message: "Request failed.",
+                error: error,
+            }
+        );
+    }
+}
+
+exports.getTasksByUser = async (req, res) =>
+{
+    try
+    {
+        const tasks = await taskModel.find({user: req.params.user});
+        res.status(200).json(
+            {
+                message: "Fetched all tasks from user.",
+                data:
+                {
+                    count: tasks.length,
+                    tasks
+                },
+            }
+        );
+    }
+    catch (error)
+    {
+        res.status(400).json(
+            {
+                message: "Request failed.",
+                error: error,
+            }
+        );
+    }
+}
--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -1,7 +1,9 @@
-const { createTask, getAllTasks } = require("../controllers/taskController");
+const { createTask, getAllTasks, getTaskById, updateTask, deleteTask, getTasksByUser } = require("../controllers/taskController");
 
 const router = require("express").Router();
 
 router.route('/').post(createTask).get(getAllTasks);
+router.route('/:id').get(getTaskById).patch(updateTask).delete(deleteTask);
+router.get('/user/:user', getTasksByUser);
 
 module.exports = router;
